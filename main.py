606f13f8ad52760febddd25fc50f7b57a8377042
Nazar <nazarhuzner@gmail.com>
2022-11-18 20:31:20 +0200

    основа

---
+++ main.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+
+na = input('Enter 1 for create\nEnter 2 for delete\nEnter 0 for check\n')
+connection = sqlite3.connect('products.sl3', 5)
+cur = connection.cursor()
+#cur.execute('CREATE TABLE productss (name TEXT, price TEXT, number TEXT)')
+#connection.commit()
