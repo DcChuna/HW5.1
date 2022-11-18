bdc0fc9c34bf1eb21600698c7643368787143204
Nazar <nazarhuzner@gmail.com>
2022-11-18 20:32:54 +0200

    іфи

--- main.py
+++ main.py
@@ -12,3 +12,17 @@ def pe():
       global n1
       global n2
       global no
+      if na =='1':
+            n0 = input('Enter name')
+            n1 = int(input('Enter price'))
+            n2 = int(input('Enter number'))
+            print('GG')
+            cur.execute(f'INSERT INTO productss (name, price, number) VALUES ("{n0}" , "{n1}" , "{n2}")')
+            connection.commit()
+      if na =='2':
+            no = input('Enter row id')
+            cur.execute(f'DELETE FROM productss WHERE rowid = {no}')
+            connection.commit()
+      if na == '0':
+            print(res)
+
