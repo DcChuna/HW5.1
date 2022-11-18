87c81dd42132354374962cd352d9d64881d43476
Nazar <nazarhuzner@gmail.com>
2022-11-18 20:32:24 +0200

    глобали

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ connection = sqlite3.connect('products.sl3', 5)
 cur = connection.cursor()
 #cur.execute('CREATE TABLE productss (name TEXT, price TEXT, number TEXT)')
 #connection.commit()
+
+def pe():
+      global n0
+      global n1
+      global n2
+      global no
